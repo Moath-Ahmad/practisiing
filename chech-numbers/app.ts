284400471efacfce94c39c8772154f1e9746cba2
Moath <m.ahmad@sit-mena.com>
2022-02-27 23:15:20 +0300

    adding if statment

---
+++ chech-numbers/app.ts
@@ -0,0 +1,15 @@
+// checking if the numbers is positve.
+
+function isPostive(num) {
+    if (num > 0) {
+        return "Yes";
+    }
+    else if (num < 0) {
+        return " Negtive error";
+    }
+    else {
+        return 'Zero error';
+    }
+};
+
+console.log(isPostive(7));
