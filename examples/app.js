f1d5ad44b0d8ff48ad22b23e927d1507ecfb65a8
Moath <m.ahmad@sit-mena.com>
2022-03-03 13:33:06 +0300

    Checking the new enhancement on es6

---
+++ examples/app.js
@@ -0,0 +1,17 @@
+var bar = 'bar';
+console.log(bar);
+bar.toUpperCase();
+console.log(bar);
+bar = bar.toUpperCase();
+console.log(bar);
+console.log('-------------------');
+var foo = [];
+console.log(foo);
+foo.push('12334');
+console.log(foo);
+foo[0].toUpperCase();
+console.log(foo);
+foo[0] = foo[0].toUpperCase();
+console.log(foo);
+console.log(Symbol(foo[0]));
+console.log(foo.valueOf());
---
+++ examples/object-literal.js
@@ -0,0 +1,24 @@
+//Object enhancement in es6 --> you can add global scop and form them into Javascript object.
+// var firstName = 'Duke';
+// var color = 'Brown';
+// var age = 5;
+// You can create object methods with object literal enhancement, plus to that we can you this as well to access the object key
+// var bark = function () {
+//     console.log(`Woof Woof!, I am ${this.firstName} and I am a ${this.age} years old, ${this.color} coloured dog.
+//     Woof Woof!`);
+// }
+// var dog = { firstName, color, age };
+// console.log(dog)
+// var anotherDog = { firstName, color, age, bark };
+// anotherDog.bark();
+//there is no necessary to use the function keyword.
+var driver = {
+    name: 'Moath',
+    speed: 100,
+    car: 'KIA RIO',
+    speedUP: function (speedup) {
+        this.speed = this.speed + speedup;
+        console.log("new speed = ".concat(this.speed));
+    }
+};
+driver.speedUP(90);
